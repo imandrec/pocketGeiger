29d235f5a7a2b0e0f820609681cde214f9483057
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2023-03-06 11:26:35 -0500

    Update pocket_geiger.py

--- pocket_geiger.py
+++ pocket_geiger.py
@@ -1,45 +1,16 @@
-#POCKET GEIGER - M4
-#+V-5V
-#GND-GND
-#SIG-D5
-#NS-D6
-
 import time
 import board
-import pulseio
-import digitalio
+import analogio
 
-# Set up the pulse input pin
-pulse_in = pulseio.PulseIn(board.D5, maxlen=100, idle_state=True)
+analog_in = analogio.AnalogIn(board.A1)
 
-# Set up the NS input pin
-ns_pin = digitalio.DigitalInOut(board.D6)
-ns_pin.direction = digitalio.Direction.INPUT
+reference_voltage = 5 # voltage supplied must be 5V
+conversion_factor = 0.0082 # SEN-14209 model, the conversion factor is given as 0.0082 µSv/hr/V.
 
-# Define a function to convert pulse frequency to radiation level
-def pulse_to_radiation(pulses_per_second):
-    CPM = pulses_per_second * 60
-    radiation_level = CPM / 334
-    return radiation_level
-
-# Main loop
 while True:
-    # Read the pulse input and calculate the frequency
-    pulse_in.clear()
-    time.sleep(1)
-    pulses_per_second = len(pulse_in) / 1.0
+    analog_input_value = analog_in.value
+    voltage = (analog_input_value / 65535) * reference_voltage
+    radiation_level = voltage * conversion_factor
 
-    # Convert the pulse frequency to radiation level
-    radiation_level = pulse_to_radiation(pulses_per_second)
-
-    # Read the NS pin and print the background radiation level
-    if ns_pin.value:
-        print("Background radiation detected")
-    else:
-        print("No background radiation detected")
-
-    # Print the radiation level
-    print("Radiation level: {:.2f} uSv/h".format(radiation_level))
-
-    # Wait for 1 second before taking the next measurement
+    print("Radiation level:", radiation_level, "µSv/hr")
     time.sleep(1)
