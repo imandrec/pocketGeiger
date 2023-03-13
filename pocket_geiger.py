6c7547780676ca5c4f808043ae20eb1302fa3134
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2023-03-13 11:19:30 -0400

    Update pocket_geiger.py

--- pocket_geiger.py
+++ pocket_geiger.py
@@ -1,164 +1,44 @@
 import time
 import board
+import analogio
 import digitalio
 
-#　Digital I/O PIN Settings　///
+sensor_pin = analogio.AnalogIn(board.A0) # analog input pin connected to the SEN-14209
+ns_pin = digitalio.DigitalInOut(board.D0) # digital input pin connected to the NS pin of the SEN-14209
+ns_pin.switch_to_input(pull=digitalio.Pull.DOWN) # set the NS pin as an input with a pull-down resistor
 
-#PIN setting for Radiation Pulse
-Rad = digitalio.DigitalInOut(board.D2); #//Radiation Pulse 
-Rad.direction = digitalio.Direction.INPUT #Vibration Noise Pulse 
+history = [0] * 6  # initialize history array to store counts for the last 6 seconds
+history_index = 0  # initialize history index variable
+count = 0  # initialize count variable
 
-#PIN setting for Noise Pulse
-Noise = digitalio.DigitalInOut(board.D3);
-Noise.direction = digitalio.Direction.INPUT
+K_ALPHA = 53.032
 
-alpha = 53.032 # cpm = uSv x alpha
-
-index = 0 # Number of loops
-
-signCount = 0 #//Counter for Radiation Pulse
-noiseCount = 0 #//Counter for Noise Pulse
-
-sON = 0 #//Lock flag for Radiation Pulse
-nON = 0 #//Lock flag for Noise Puls
-
-cpm = 0; #//Count rate [cpm] of current
-cpmHistory= [] #//History of count rates
-cpmIndex = 0 #//Position of current count rate on cpmHistory[]
-cpmIndexPrev = 0 #//Flag to prevent duplicative counting
-
-#Timing Settings for Loop Interval
-prevTime = 0
-currTime = 0
-
-totalSec = 0 #Elapsed time of measurement [sec]
-totalHour = 0 #Elapsed time of measurement [hour]
-
-#Time settings for CPM calcuaration
-cpmTimeMSec = 0
-cpmTimeSec = 0
-cpmTimeMin = 0
-
-#String buffers of float values for serial output
-#unlimited empty buffer 
-cpmBuff = []
-uSvBuff = []
-uSvdBuff = []
-
-#CSV-formatting for serial output (substitute , for _)
-print("hour[h]_sec[s]_count_cpm_uSv/h_uSv/hError");
-
-#Initialize cpmHistory[]
-cpmHistory = [0 for i in range(200)]
-
-#Get start time of a loop
-prevTime = time.time();
+start_time = time.monotonic()  # initialize start time
+cpm_avg = 0  # initialize average CPM variable
+avg_timer = time.monotonic()  # initialize timer for updating average CPM
 
 while True:
+    if ns_pin.value == 0: # if NS pin is low (no noise detected)
+        reading = sensor_pin.value # read the raw ADC value
+        voltage = reading * 5 / 65535 # convert to voltage
+        cpm = voltage / 0.0057 # convert voltage to CPM using the SEN-14209's conversion factor
 
-  #Raw data of Radiation Pulse: Not-detected -> High, Detected -> Low
-  sign = Rad.value
+        if cpm < 20:
+            count += 1  # increment count if cpm is less than 20
 
-  #Raw data of Noise Pulse: Not-detected -> Low, Detected -> High
-  noise = Noise.value
+        history[history_index] = count  # store count in history array
+        history_index = (history_index + 1) % len(history)  # increment history index, wrapping around if necessary
+        count = 0  # reset count
 
-  #Radiation Pulse normally keeps low for about 100[usec]
-  if sign == 0 and sON == 0:
-   #//Deactivate Radiation Pulse counting for a while
-    sON = 1
-    signCount=+1
-  elif sign == 1 and sON == 1:
-    sON = 0
+        if time.monotonic() - avg_timer >= 1.0:  # if 1 second has elapsed since last update
+            cpm_avg = sum(history) / len(history)  # calculate average CPM over the last 6 seconds
+            avg_timer = time.monotonic()  # reset timer for updating average CPM
 
+        uSv_h = cpm_avg * K_ALPHA / 60.0
 
-  #Noise Pulse normally keeps high for about 100[usec]
-  if noise == 1 and nON == 0:
-   #Deactivate Noise Pulse counting for a while
-    nON = 1
-    noiseCount=+1
-  elif noise == 0 and nON == 1:
-    nON = 0
+        print("uSv/h: {0:.2f}".format(uSv_h))  # print0 radiation value and CPM average
 
+        start_time = time.monotonic()  # reset start time
 
-  #Output readings to serial port, after 10000 loops
-  if index == 10000:#//About 160-170 msec in Arduino Nano(ATmega328)
+    time.sleep(0.16) # wait for 160ms before taking the next reading
 
-    #Get current time
-    currTime = time.time()
-
-    #No noise detected in 10000 loops
-    if noiseCount == 0:
-
-      #Shift an array for counting log for each 6 sec.
-      if totalSec % 6 == 0 and cpmIndexPrev and not totalSec:
-
-        cpmIndexPrev = totalSec
-        cpmIndex=+1
-
-        if cpmIndex >= 200:
-
-          cpmIndex = 0
-
-
-        if cpmHistory[cpmIndex] > 0:
-
-          cpm -= cpmHistory[cpmIndex]
-
-        cpmHistory[cpmIndex] = 0
-
-
-      #Store count log
-      cpmHistory[cpmIndex] += signCount
-      #Add number of counts
-      cpm += signCount
-
-      #Get ready time for 10000 loops
-      cpmTimeMSec += abs(currTime - prevTime)
-      #Transform from msec. to sec. (to prevent overflow)
-      if cpmTimeMSec >= 1000:
-
-        cpmTimeMSec -= 1000
-        #Add measurement time to calcurate cpm readings (max=20min.)
-        if cpmTimeSec >= 20 * 60:
-
-          cpmTimeSec = 20 * 60;
-        else:
-          cpmTimeSec+=1
-
-
-        #Total measurement time
-        totalSec=+1
-        #Transform from sec. to hour. (to prevent overflow)
-        if totalSec >= 3600:
-
-          totalSec -= 3600
-          totalHour+=1
-
-      min = cpmTimeSec / 60.0
-
-      if min != 0:
-    # Calculate cpm, uSv/h and error of uSv/h
-        cpmBuff = "{:.3f}".format(cpm / min)
-        uSvBuff = "{:.3f}".format(cpm / min / alpha)
-        uSvdBuff = "{:.3f}".format(math.sqrt(cpm) / min / alpha)
-
-      else:
-    # Division by zero
-        cpmBuff = "0.000"
-        uSvBuff = "0.000"
-        uSvdBuff = "0.000"
-    
-    print("%d,%d.%03d,%d,%s,%s,%s" % (
-          totalHour, totalSec, cpmTimeMSec, signCount,
-          cpmBuff, uSvBuff, uSvdBuff
-        )
-     )
-
-
-    #Initialization for next 10000 loops
-    prevTime = currTime
-    signCount = 0
-    noiseCount = 0
-    index = 0
-
-  index+=1
