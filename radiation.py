cf4ac2d99dc137814f11adff08348d88d6b0afdf
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2023-03-13 13:18:13 -0400

    Create radiation.py

---
+++ radiation.py
@@ -0,0 +1,43 @@
+import time
+import board
+import analogio
+import digitalio
+
+# Define constants for radiation sensor and history buffer
+K_BETA = 0.0875  # conversion factor for SEN-14209 radiation sensor, in uSv/h per count per second
+BUFFER_LENGTH = 60  # maximum number of radiation values that can be stored in the buffer
+BUFFER_UNIT = 1  # interval of time between buffer updates, in seconds
+
+# Initialize sensor pin and NS pin
+sensor_pin = analogio.AnalogIn(board.A0)
+ns_pin = digitalio.DigitalInOut(board.D7)
+ns_pin.switch_to_input(pull=digitalio.Pull.DOWN)
+
+# Initialize variables for radiation readings and buffer
+total_uSv_h = 0.0
+count = 0
+buffer = [0] * BUFFER_LENGTH
+bufferIndex = 0
+previousBufferTime = int(time.monotonic() * 1000)
+
+def get_total_radiation():
+    global total_uSv_h, count, buffer, bufferIndex, previousBufferTime
+
+    if ns_pin.value == 0: # if NS pin is low (no noise detected)
+        reading = sensor_pin.value # read the raw ADC value
+        voltage = reading * 5 / 65535 # convert to voltage
+        cpm = voltage / 0.0057 # convert voltage to CPM using the SEN-14209's conversion factor
+
+        if cpm < 20:
+            count += 1
+            uSv_h = count * K_BETA / 60.0  # calculate uSv/h with the conversion factor in microsieverts
+            total_uSv_h += uSv_h
+
+            # Update buffer if enough time has passed
+            currentTime = int(time.monotonic() * 1000)
+            if (currentTime - previousBufferTime >= BUFFER_UNIT * 1000):
+                previousBufferTime += BUFFER_UNIT * 1000
+                bufferIndex = (bufferIndex + 1) % BUFFER_LENGTH
+                buffer[bufferIndex] = uSv_h
+
+    return total_uSv_h
