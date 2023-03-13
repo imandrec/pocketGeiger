731ebfd0565ee64a28e92f79c950049135e72ae4
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2023-03-13 14:15:56 -0400

    Update pocket_geiger.py

--- pocket_geiger.py
+++ pocket_geiger.py
@@ -3,8 +3,7 @@ import time
 
 while True:
     total_uSv_h = radiation.get_total_radiation()
-    print("Total uSv/h: {0:.3f}".format(total_uSv_h))  # print total radiation value in microsieverts
-    time.sleep(0.3)
-
+    print("Total uSv/h: {0:.4f}".format(total_uSv_h))  # print total radiation value in microsieverts
+    time.sleep(0.6)
 
 
